1b91ee0d8233aa513f7e279da245b29ca96ac9ce
leoking <Leoking470@gmail.com>
2020-08-28 18:46:26 +0300

    added Rect

---
+++ Engine/Rect.cpp
@@ -0,0 +1,36 @@
+#include "Rect.h"
+
+Rect::Rect(Vector2 topLeft, int width, int height)
+	:
+	topLeft(topLeft),
+	width(width),
+	height(height)
+{
+}
+
+void Rect::Draw(Graphics& gfx, Color c) const
+{
+	gfx.DrawRect(topLeft.x, topLeft.y, width, height, c);
+}
+
+bool Rect::isIntersecting(const Rect& other) const
+{
+	return topLeft.x < other.topLeft.x + other.width &&
+		topLeft.x + width > other.topLeft.x &&
+		topLeft.y < other.topLeft.y + other.height &&
+		topLeft.y + height > other.topLeft.y;
+}
+
+void Rect::Translate(const Vector2& delta)
+{
+	topLeft += delta;
+	ClampPositionToScreen();
+}
+
+void Rect::ClampPositionToScreen()
+{
+	if (topLeft.x <= 0) { topLeft.x = 0.0f; }
+	if (topLeft.x + width >= Graphics::ScreenWidth)   { topLeft.x = float(Graphics::ScreenWidth - width); }
+	if (topLeft.y <= 0) { topLeft.y = 0.0f; }
+	if (topLeft.y + height >= Graphics::ScreenHeight) { topLeft.y = float(Graphics::ScreenHeight - height); }
+}
---
+++ Engine/Rect.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "Graphics.h"
+#include "Vector2.h"
+
+class Rect
+{
+private:
+	Vector2 topLeft;
+	int width;
+	int height;
+	
+	void ClampPositionToScreen();
+public:
+	Rect(Vector2 topLeft, int width, int height);
+
+	void Draw(Graphics& gfx, Color c) const;
+	bool isIntersecting(const Rect& Paddle) const;
+
+	void Translate(const Vector2& delta);
+
+	// getters
+
+};
+
